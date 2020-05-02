b3b564c5a8a1d11ac95c6a1c8c2a36254e094e46
Ajay Javiya <javiyaajay1987@gmail.com>
2020-05-02 11:11:20 +0530

    [ADD] new module : partner_label_report

---
+++ partner_label_report/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright (C) 2019-Today: Druidoo (<https://www.druidoo.io>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Contact's Label Report",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Druidoo, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/partner-contact",
+    "category": "Customer Relationship Management",
+    "summary": "Print Partner Label Report",
+    "depends": ["contacts"],
+    "data": ["data/paperformat_data.xml", "views/report_partner_label.xml"],
+    "installable": True,
+}
---
+++ partner_label_report/readme/USAGE.rst
@@ -0,0 +1,1 @@
+Print Partner Label report
